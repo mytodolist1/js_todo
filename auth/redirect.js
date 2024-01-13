4628b2bd70e1fb07035a9eab199154746b6d4923
dirgafebrian <dirgafebrian45@gmail.com>
2024-01-13 23:13:39 +0700

    Add redirect functionality for authentication and delete file not use

--- auth/redirect.js
+++ auth/redirect.js
@@ -3,10 +3,6 @@ import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 const checkTokenAndRedirect = () => {
     const token = "Authorization";
     const tokenValue = getCookie(token);
-<<<<<<< HEAD:auth/redirect.js
-=======
-    // console.log("Token Value:", tokenValue);
->>>>>>> e5e20a71d7fcbec4f283ab1c94c286743a6de9d8:js/redirect.js
 
     if (!tokenValue) {
         window.location.href = "../login.html";
